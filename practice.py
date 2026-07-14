8f743c87597eeefe81199bcd1e602bf5e3ddde38
JEREMY <jeremytan1184@gmail.com>
2026-07-14 23:37:33 +0800

    test

--- practice.py
+++ practice.py
@@ -3,3 +3,4 @@ with open("note.txt","a") as my_file:
     my_file.write("testing inside the room11\n")
     my_file.write("trying to write outside the room")
 print( "text written successfully!")
+#test
